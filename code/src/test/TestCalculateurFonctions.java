457f09c73b3ccdc33f1dd5bda0333b24054171d6
Kagarie <aymeric.adenot27@orange.fr>
2022-03-07 22:40:35 +0100

    correction de la fonction pour le calcul des nombres premiers

--- code/src/test/TestCalculateurFonctions.java
+++ code/src/test/TestCalculateurFonctions.java
@@ -3,17 +3,29 @@ package test;
 import serveur.CalculateurFonctions;
 
 import java.math.BigInteger;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 
 public class TestCalculateurFonctions {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException {
         CalculateurFonctions cf = new CalculateurFonctions();
-        BigInteger nbr = new BigInteger("54321");
-        BigInteger dix = new BigInteger("10");
 
-        //Test de la fonction qui fait la somme des c'est chiffre
-        while (nbr.compareTo(dix) > 0) {
-            nbr = cf.sommeBitInteger(nbr);
+        //On créait une liste dans laquelle on ajoute différente valeurs
+        ArrayList<BigInteger> listBigInteger = new ArrayList<>();
+        listBigInteger.add(BigInteger.valueOf(2));
+        listBigInteger.add(BigInteger.valueOf(3));
+        listBigInteger.add(BigInteger.valueOf(5));
+        listBigInteger.add(BigInteger.valueOf(7));
+        listBigInteger.add(BigInteger.valueOf(6));
+        listBigInteger.add(BigInteger.valueOf(999));
+        listBigInteger.add(BigInteger.valueOf(997));
+        listBigInteger.add(BigInteger.valueOf(2000));
+        listBigInteger.add(BigInteger.valueOf(2003));
+
+        //On vient tester la fonction "calculNombrePremier"
+        ArrayList<BigInteger> res = cf.calculNombrePremier(listBigInteger);
+        for(BigInteger nbr : res)
             System.out.println(nbr);
-        }
     }
 }
