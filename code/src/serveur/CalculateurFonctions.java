3887b6b336529dfa3729aaff4cfe4af67fa670c2
Kagarie <aymeric.adenot27@orange.fr>
2022-03-07 22:40:23 +0100

    correction de la fonction pour le calcul des nombres premiers

--- code/src/serveur/CalculateurFonctions.java
+++ code/src/serveur/CalculateurFonctions.java
@@ -8,39 +8,37 @@ import java.util.List;
 public class CalculateurFonctions implements calculateurFonctionsImp {
 
     /**
-     *
      * @param listeNombre
      * @return
      * @throws RemoteException
      */
     @Override
     public ArrayList<BigInteger> calculNombrePremier(List<BigInteger> listeNombre) throws RemoteException {
-        ArrayList<BigInteger> resNombrePremiers = new ArrayList<BigInteger>();
+        ArrayList<BigInteger> resNombrePremiers = new ArrayList<>();
         //On va vérifier un à un tous les nombres de notre liste
         for (BigInteger nombre : listeNombre) {
-            //Test rapide si le nombre et 2 ou 5 (cas d'execption)
-            if (BigInteger.valueOf(2).equals(nombre) || BigInteger.valueOf(5).equals(nombre))
+            //Test rapide si le nombre et 2 (cas d'execption)
+            if (BigInteger.valueOf(2).equals(nombre))
                 resNombrePremiers.add(nombre);
             else {
-
-                /** On boucle sur la fonction tant que le résultat obtenu n'est pas plus petit que 10
-                * La méthode compare tout renvoi -1 0 1 respectivement pour inférieur, égale et supérieur
-                * Si on passe 54321 le résultat sera 6
-                * */
-                while (nombre.compareTo(BigInteger.valueOf(10)) > 0)
-                    nombre = this.sommeBitInteger(nombre);
-                //On reprends le nombre obtenu précédement et on l'ajoute s'il respecte bien les régles pour un nombre premier
-                if (BigInteger.valueOf(1).equals(nombre) || BigInteger.valueOf(3).equals(nombre) || BigInteger.valueOf(7).equals(nombre) || BigInteger.valueOf(9).equals(nombre))
+                //Boolean qui défini si un nombre et premier ou non
+                boolean notPrimeNumber = false;
+                //Calcul de la racine carré du nombre qui nous servira de limite
+                int limite = (int) (Math.sqrt(nombre.doubleValue()) + 1);
+                /*On test tous les nombres jusqu'à la limite (la racine)
+                / On s'arrête à la racine car au dessus on est sur qu'il est premier
+                 */
+                for (int i = 3; i < limite; i++) {
+                    //Si on obtiens un divisions sans reste le nombre n'est pas premiers donc il ne sera pas ajouté
+                    if (nombre.mod(BigInteger.valueOf(i)).compareTo(BigInteger.valueOf(0)) == 0) {
+                        notPrimeNumber = true;
+                    }
+                }
+                //Si le nombre n'est pas premier on le retient pas
+                if (!notPrimeNumber)
                     resNombrePremiers.add(nombre);
             }
         }
         return resNombrePremiers;
-    }
-
-    /**
-     *Permet de faire la somme des nombres présent dans un btiInteger ex : 54321 => 15
-     */
-    public BigInteger sommeBitInteger(BigInteger nombre) {
-        return BigInteger.valueOf(0).equals(nombre) ? BigInteger.valueOf(0) : nombre.mod(BigInteger.valueOf(10)).add(sommeBitInteger(nombre.divide(BigInteger.valueOf(10))));
     }
 }
